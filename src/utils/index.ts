97c47c3f571cacd5b80cc9f3d3df002b803337e3
ESnark <settimeout1000@gmail.com>
2026-02-05 14:04:30 +0900

    fix: only use pino-pretty in development mode

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import pino from 'pino';
 
 export const logger = pino({
   level: process.env.PROXMOX_LOG_LEVEL || 'info',
-  transport: process.env.NODE_ENV !== 'production' ? {
+  transport: process.env.NODE_ENV === 'development' ? {
     target: 'pino-pretty',
   } : undefined,
 });
